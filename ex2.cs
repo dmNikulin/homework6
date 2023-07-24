900f1a760ef87951b5cae95073ecb3acc6861fb5
Dmitrii Nikulin <138107026+dmNikulin@users.noreply.github.com>
2023-07-24 21:08:43 +0300

    Create ex2.cs

---
+++ ex2.cs
@@ -0,0 +1,21 @@
+//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+Console.Write("Введите значение b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double z1 = k1 - k2; // -4
+double z2 = b1 - b2; // -2
+double x = z2 / (-z1);
+double y = k2 * x + b2;
+
+Console.WriteLine($"{Math.Round(x,2)}, {Math.Round(y,2)}");
